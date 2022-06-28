33f11247728c2f2e490c33b555130f4a4db4dd25
thomastran2k2 <80449239+thomastran2k2@users.noreply.github.com>
2022-06-28 22:49:47 +1000

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import tkinter as  tk
 
 LIGHT_GRAY = "#F5F5F5"
 LABEL_COLOR = "#25265E"
+WHITE = "#FFFFFF"
+OFF_WHITE = "#F8FAFF"
 SMALL_FONT_SIZE = ("Arial", 16)
 BIG_FONT_SIZE = ("Arial", 40, "bold ")
+DIGITS_FONT_SIZE = ("Arial", 24, "bold")
+DEFAULT_FONT_SIZE = ("Arial", 20)
+
 class Calculator:
     def __init__(self):
         self.window = tk.Tk()
@@ -14,9 +19,16 @@ class Calculator:
         self.total_expression = "0"
         self.display_frame = self.create_display_frame()
         self.total_label, self.label = self.create_display_labels()
-
+        self.digits = {
+            7:(1,1), 8:(1,2), 9:(1,3),
+            4:(2,1), 5:(2,2), 6:(2,3),
+            1:(3,1), 2:(3,2), 3:(3,3),
+            0:(4,2), ".":(4,1)
+        }
+        self.operations = {"/" : "\u00F7", "*" : "\u00D7", "-": "-", "+" : "+"}
         self.buttons_frame = self.create_buttons_frame()
-
+        self.create_digit_buttons()
+        self.create_operator_buttons()
     def create_display_labels(self):
         total_label = tk.Label(self.display_frame, text = self.total_expression, anchor=tk.E, bg = LIGHT_GRAY, fg= LABEL_COLOR, padx = 24, font = SMALL_FONT_SIZE)
         total_label.pack(expand =True, fill = "both")
@@ -26,11 +38,23 @@ class Calculator:
         return total_label, label
 
 
+    def create_operator_buttons(self):
+        i = 0
+        for operator,symbol in self.operations.items():
+            button = tk.Button(self.buttons_frame, text = symbol, bg= OFF_WHITE, fg = LABEL_COLOR, font = DEFAULT_FONT_SIZE, borderwidth = 0)
+            button.grid(row = i, column = 4, sticky = tk.NSEW)
+            i += 1
 
     def create_display_frame(self):
         frame = tk.Frame(self.window, height = 221, bg = LIGHT_GRAY)
         frame.pack(expand = True, fill = "both")
         return frame
+    def create_digit_buttons(self):
+        for digit,grid_value in self.digits.items():
+            button = tk.Button(self.buttons_frame, text = str(digit), bg = WHITE, fg = LABEL_COLOR, font = DIGITS_FONT_SIZE)
+            button.grid(row = grid_value[0], column = grid_value[1], sticky = tk.NSEW)
+
+
     def create_buttons_frame(self):
         frame = tk.Frame(self.window)
         frame.pack(expand = True, fill = "both")
